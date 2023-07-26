bd2af97ab859ad0a83e4c75cadf6d82aa2d6c1ca
Yoo Sung Jae <rimuse0207@naver.com>
2023-07-26 12:58:35 +0900

    주차 settimeout 설정

--- src/Components/Car_Contact/CarContact.js
+++ src/Components/Car_Contact/CarContact.js
@@ -35,6 +35,22 @@ const CarContactMainDivBox = styled.div`
 
 const CarContact = () => {
     const dispatch = useDispatch();
+    const [Car_List, setCar_List] = useState([
+        { company_car_epid :'M220511063838A347157', company_car_name:'155허 7879', company_car_explain:'K3'},
+        { company_car_epid :'M220511065112A342288', company_car_name:'191허 3152', company_car_explain:'아반떼'},
+        { company_car_epid :'M220511065222A346350', company_car_name:'45호 6140', company_car_explain:'아반떼 AD'},
+        { company_car_epid :'M220511065331A342699', company_car_name:'45호 6141', company_car_explain:'아반떼 AD'},
+        { company_car_epid :'M220511065431A343884', company_car_name:'45호 6142', company_car_explain:'아반떼 AD'},
+        { company_car_epid :'M220511063942A348400', company_car_name:'155허 7880', company_car_explain:'K3'},
+        { company_car_epid :'M220511064109A345960', company_car_name:'155허 8053', company_car_explain:'K3'},
+        { company_car_epid :'M220511064156A344713', company_car_name:'155허 7765', company_car_explain:'K3'},
+        { company_car_epid :'M220511064243A342636', company_car_name:'14나 1878', company_car_explain:'YF 소나타'},
+        { company_car_epid :'M220511064348A348000', company_car_name:'191허 3153', company_car_explain:'아반떼'},
+        { company_car_epid :'M220511064441A34933', company_car_name:'191허 3655', company_car_explain:'아반떼'},
+        { company_car_epid :'M220511064534A346135', company_car_name:'45호 6144', company_car_explain:'아반떼 AD'},
+        { company_car_epid :'M220511065002A343682', company_car_name:'74러 3874', company_car_explain:'스타렉스'},
+
+    ]);
     const {car_Target_ID,car_User_ID} = useParams();
     const [PhoneNumber, setPhoneNumber] = useState("+82-31-639-9026");
     const Loading = useSelector(state => state.LoaderCheckingRedux.loading);
@@ -47,7 +63,9 @@ const CarContact = () => {
                     car_User_ID
                 }
             });            
-            console.log(Contact_Reservation_Axios);
+             setTimeout(() => {
+                dispatch(Loader_Check_For_False());
+            },100000)
             if (Contact_Reservation_Axios.data.dataSuccess) {
                 if (Contact_Reservation_Axios.data.dataNothing) {
                     setPhoneNumber("+82-31-639-9026");
@@ -75,20 +93,20 @@ const CarContact = () => {
 
     useEffect(() => {
         Contact_Reservation(); 
+       
     },[])
 
     return (
         <CarContactMainDivBox>
-            <div style={{ marginTop: "80px" }}>
-                <h2>불편함을 드려 죄송합니다.</h2>
+            { !Loading ? <div>
+                <div style={{ marginTop: "80px" }}>
+                    <h2>{ Car_List.map((list)=>list.company_car_epid === car_Target_ID ? `${list.company_car_explain} : ${list.company_car_name}` :"" )}</h2>
+                <h2>불편함을 끼쳐드려 죄송합니다.</h2>
             </div>
             <div className="Content_Container">
-             
                 <a href={`tel:${PhoneNumber}`}>
                  <div className="Phone_Icon_Container">
-                    
-                            <BsFillTelephoneOutboundFill></BsFillTelephoneOutboundFill>
-                    
+                        <BsFillTelephoneOutboundFill></BsFillTelephoneOutboundFill>
                     </div>
                     </a>
                 <div style={{lineHeight:"30px",fontSize:"1.2em",marginTop:"40px"}}>
@@ -97,7 +115,9 @@ const CarContact = () => {
                     <div><strong>전화연결</strong> 화면으로 연결됩니다.</div>
                 </div>
                
-            </div>
+                </div>
+            </div>:<div></div>}
+           
             {/* 로딩 컴포넌트 시작 */}
             <LoaderMainPage loading={Loading}></LoaderMainPage>
         </CarContactMainDivBox>
